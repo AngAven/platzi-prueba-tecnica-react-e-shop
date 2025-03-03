6317e918f2215fe92d7f1d0bc0a3604cca40e32c
Angel Avendaño Cruz <angel.aven.c@gmail.com>
2025-03-03 15:39:12 -0600

    Github pages configuration

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: 'https://angaven.github.io/platzi-prueba-tecnica-react-e-shop/',
   plugins: [react()],
 })
